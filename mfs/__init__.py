6e54c25a2e96339848101f86c3381b87b70aa19d
Nicholas St. Clair <nstclair@ucmerced.edu>
2021-09-22 16:25:52 -0700

    Update __init__.py

--- mfs/__init__.py
+++ mfs/__init__.py
@@ -414,7 +414,7 @@ class Scatter:
             # Iterate over incoming planewaves
             for A, k in zip(A_inc, k_inc):
                 ik = 1j * k
-                ff = A * np.exp(dot1(ik, X)) * np.exp(-s**2/sigma**2)
+                ff = A * np.exp(dot1(ik, X)) * np.exp(s**2/sigma**2)
                 f += ff
 
                 if grad:
